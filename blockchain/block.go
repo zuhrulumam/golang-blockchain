31ec895707e9f2de3b91255330434b200c2dbdf3
Muhammad Zuhrul Umam <zuhrulu@gmail.com>
2018-12-24 21:26:04 +0700

    basic blockchain

---
+++ blockchain/block.go
@@ -0,0 +1,60 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+// InitChain init block chain with genesis block
+func InitChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+// Genesis iniial block for the chain
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+// AddBlock add block to a blockchain
+func (chain *BlockChain) AddBlock(data string) {
+	// get previous block
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+
+	// get previous hash from previous chain
+	newBlock := CreateBlock(data, prevBlock.Hash)
+
+	chain.Blocks = append(chain.Blocks, newBlock)
+}
+
+// BlockChain model
+type BlockChain struct {
+	Blocks []*Block
+}
+
+// Block model
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+// CreateHash create hash on particular block
+func (b *Block) CreateHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+// CreateBlock create block with hash, data, prevhash
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{
+		Hash:     []byte{},
+		Data:     []byte(data),
+		PrevHash: prevHash,
+	}
+
+	block.CreateHash()
+
+	return block
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/zuhrulumam/golang-blockchain/blockchain"
+)
+
 func main() {
 
+	chain := blockchain.InitChain()
+
+	chain.AddBlock("Test")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Data %s \n", block.Data)
+		fmt.Printf("prevHash %x \n", block.PrevHash)
+		fmt.Printf("Hash %x \n", block.Hash)
+	}
 }
